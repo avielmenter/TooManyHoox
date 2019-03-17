b8caf06c6e97fd46ea9e1054bd65cb1d02f2f1b7
avielmenter <aviel.menter@gmail.com>
2019-03-17 04:53:29 -0400

    renamed and documented project

--- README.md
+++ README.md
@@ -1,2 +1,119 @@
-# hoox
+# TooManyHoox
 Simple state management using React's hooks and context API
+
+# About
+
+TooManyHoox is a simple state management library created to use React's hooks and context APIs. Hoox is similar to libraries like Redux, except that it is invoked via a custom hook, rather than through a component decorator.
+
+# Examples
+
+TooManyHoox can be used either to create a state management pattern within a single component, or provide access to a store across many components using the context API.
+
+## Single Component
+
+```javascript
+import { useStore, useHoox } from 'toomanyhoox';
+
+function reducer(state, action) {
+	switch (action.type) {
+		case 'SpoilTheBroth':
+			return ...;
+
+		// . . . 
+
+		default:
+			return state;
+	}
+}
+
+const mapStateToProps = (state) => ({
+	message: state.text
+})
+
+const mapDispatchToProps = (dispatch) => ({
+	someAction: (params) => dispatch({
+		type: "SpoilTheBroth",
+		params
+	}),
+	// . . . 
+});
+
+const component = (props) => {
+	const store = useStore(
+		reducer,
+		{ text: "initial state" }
+	);
+
+	const { state, actions } = useHoox(
+		store, 
+		mapStateToProps, 
+		mapDispatchToProps
+	);
+
+	// . . .
+}
+```
+
+## Multiple Components
+
+First, create the store.
+
+```javascript
+import { createStore } from 'toomanyhoox';
+
+const { store, useStore } = createStore(
+	reducer,
+	{ text: "initial state" }
+);
+```
+
+Then, wrap the top of the component tree in a context provider. This step is necessary for the store to be accessible in components within the wrapped component.
+
+```javascript
+import { HooxProvider } from 'toomanyhoox';
+
+const App = (props) => (
+	<HooxProvider store={store}>
+		<!-- . . . -->
+	</HooxProvider>
+);
+```
+
+Now, the store can be accessed from functional components contained within the provider.
+
+```javascript
+const component = (props) => {
+	const { state, actions } = useStore(
+		mapStateToProps,
+		mapDispatchToProps
+	);
+
+	// . . .
+}
+```
+
+## Middleware
+
+Middleware can be used to add custom functionality to the dispatch function. Middleware functions take the current state, action, and dispatch function, and return a new dispatch function.
+
+This example middleware writes the type of every action dispatched to the console.
+
+```javascript
+const logger = (state) => (dispatch) => (action) => {
+	console.log(action.type);
+	dispatch(action);
+}
+```
+
+The logger can then be added to any store when it is created.
+
+```javascript
+const { store, useStore } = createStore(
+		reducer,
+		initialState,
+		[logger, /* . . .*/]
+);
+```
+
+# License
+This project is licensed under the [MIT License](https://github.com/avielmenter/toomanyhoox/blob/master/LICENSE).
---
+++ src/index.tsx
@@ -0,0 +1,204 @@
+import * as React from 'react';
+
+// TYPE DEFINITIONS
+
+/**
+ * Takes a state and an action, and produces a new state.
+ * @typedef {(state: State, action: Action) => State} Reducer<State,Action>
+ * @param {State} State The type of the state accepted and produced by the reducer
+ * @param {Action} Action The type of actions can modify the state
+ */
+export type Reducer<State, Action> = (state: State, action: Action) => State;
+
+/**
+ * The type of the dispatch function used to dispatch an action
+ * @typedef {React.Dispatch<Action>} Dispatch<Action>
+ * @param {Action} Action The type of actions that can be dispatched
+ */
+export type Dispatch<Action> = React.Dispatch<Action>;
+
+/**
+ * The type of a middleware function that can process an action before dispatchment
+ * @typedef {(state: State) => (dispatch: Dispatch<Action>) => Dispatch<Action>} Middleware<State,Action>
+ * @param {State} State The type of state accepted and produced by the reducer
+ * @param {Action} Action The type of actions can modify the state
+ */
+export type Middleware<State, Action> = (state: State)
+	=> (dispatch: Dispatch<Action>)
+		=> Dispatch<Action>;
+
+/**
+ * Contains the current state, and a dispatch functions used to modify the state
+ * @typedef {state: State, dispatch: Dispatch} Store<State,Action>
+ * @param {State} State the type of state accepted and produced by the reducer
+ * @param {Action} Action the type of actions can modify the state
+ */
+export type Store<State, Action> = {
+	state: State,
+	dispatch: Dispatch<Action>
+}
+
+/**
+ * Contains the current state, and an object with functions used to dispatch actions
+ * @typedef {State: State, actions: DispatchProps} StoreProps<State,DispatchProps>
+ * @param {State} State the type of state accepted and produced by the reducer
+ * @param {DispatchProps} DispatchProps the object with functions used to dispatch actions
+ */
+export type StoreProps<State, DispatchProps> = {
+	state: State,
+	actions: DispatchProps
+}
+
+/**
+ * The type of the React context associated with the store
+ * @typedef {React.Context<Store<State,Action>>} StoreContext<State,Action>
+ * @param {State} State the type of state accepted and produced by the reducer
+ * @param {Action} Action the type of actions that can modify the state
+ */
+type StoreContext<State, Action> = React.Context<Store<State, Action>>;
+
+/**
+ * Contains the configuration information for the store
+ * @typedef {reducer: Reducer<State,Action>, state: State, middleware: Middleware<State,Action> | Middleware<State,Action>[] | undefined, context: StoreContext<State,Action>} StoreConfig<State,Action>
+ * @param {State} State the type of state accepted and produced by the reducer
+ * @param {Action} Action the type of actions that can modify the state
+ */
+type StoreConfig<State, Action> = {
+	reducer: Reducer<State, Action>,
+	state: State,
+	middleware?: Middleware<State, Action> | Middleware<State, Action>[],
+	context: StoreContext<State, Action>
+};
+
+// UTIL
+
+/**
+ * Combines reducers for different properties of the state into a single reducer for the whole state
+ * @param reducers An object with a reducer for each property of the state
+ * @returns {Reducer<State,Action>} The combined reducer
+ */
+export function combineReducers<S, A>(reducers: { [K in keyof S]: Reducer<S, A> }): Reducer<S, A> {
+	return (s: S, a: A) => {
+		let state = s;
+
+		for (const k in reducers) {
+			state = reducers[k](state, a);
+		}
+
+		return state;
+	}
+}
+
+/**
+ * Return this from middleware instead of a dispatch function when you want nothing to happen
+ */
+export function NoOp<Action>(_: Action): void { };
+
+// REDUCER CUSTOM HOOKS
+
+/**
+ * Custom hook to create a store
+ * @param {Reducer<State,Action>} reducer The reducer for the store
+ * @param {State} state The initial state in the store 
+ * @param {Middleware<State,Action> | Middleware<State, Action> | undefined} middleware The middleware, if any, used in the store
+ * @returns {Store<State,Action>} The store
+ */
+export function useStore<State, Action>(
+	reducer: Reducer<State, Action>,
+	state: State,
+	middleware?: Middleware<State, Action> | Middleware<State, Action>[]
+): Store<State, Action> {
+	const [currState, defaultDispatch] = React.useReducer(
+		reducer,
+		state
+	);
+
+	const dispatch = middleware
+		? (middleware instanceof Array
+			? middleware.reduce((p, c) => c(currState)(p), defaultDispatch)
+			: middleware(currState)(defaultDispatch)
+		)
+		: defaultDispatch;
+
+	return {
+		state: currState,
+		dispatch,
+	};
+};
+
+/**
+ * Takes a store or store context and produces a store with actions to modify the state
+ * @param {Store<State,Action> | StoreContext<State,Action} store The store or store context
+ * @param {(state: State) => MappedState} mapStateToProps A function mapping the full state to the state used here
+ * @param {(dispatch: Dispatch<Action>) => DispatchProps} mapDispatchToProps A function producing the dispatch props
+ * @returns {StoreProps<MappedState, DispatchProps>} A store with a state, and an object with functions to modify the state
+ */
+export function useHoox<State, Action, DispatchProps, MappedState = State>(
+	store: Store<State, Action> | StoreContext<State, Action>,
+	mapStateToProps: (state: State) => MappedState,
+	mapDispatchToProps: (dispatch: Dispatch<Action>) => DispatchProps
+): StoreProps<MappedState, DispatchProps> {
+	const hooxStore: Store<State, Action> = (store as StoreContext<State, Action>).Consumer
+		? React.useContext(store as StoreContext<State, Action>) as Store<State, Action>
+		: store as Store<State, Action>;
+
+	return {
+		state: mapStateToProps(hooxStore.state),
+		actions: mapDispatchToProps(hooxStore.dispatch)
+	}
+}
+
+// CONTEXT CUSTOM HOOKS
+
+/**
+ * Creates a store configuration file that can be used to create a store context, and a custom hook for using the store
+ * @param {Reducer<State, Action>} reducer The reducer for the store
+ * @param {State} state The initial state in the store
+ * @param {Middleware<State,Action> | Middleware<State,Action>[] | undefined} middleware The middleware run before an action is dispatched
+ * @returns {store: StoreConfig<State,Action>, useStore: StoreHook<State, Action, DispatchProps, MappedState = State>)} The store configuration and hook 
+ */
+export function createStore<State, Action>(
+	reducer: Reducer<State, Action>,
+	state: State,
+	middleware?: Middleware<State, Action> | Middleware<State, Action>[]
+) {
+	const dispatch: React.Dispatch<Action> = NoOp;
+
+	const store: StoreConfig<State, Action> = {
+		reducer,
+		state,
+		middleware,
+		context: React.createContext({
+			state,
+			dispatch
+		})
+	};
+
+	return {
+		store,
+		useStore: <MappedState, DispatchProps>(
+			mapStateToProps: (state: State) => MappedState,
+			mapDispatchToProps: (dispatch: React.Dispatch<Action>) => DispatchProps
+		) => useHoox(React.useContext(store.context), mapStateToProps, mapDispatchToProps)
+	};
+}
+
+type ComponentProps<State, Action> = {
+	store: StoreConfig<State, Action>
+}
+
+/**
+ * The context provider for a store
+ * @param {StoreConfig<State,Action>} store The configuration for the store to provide 
+ */
+export const HooxProvider: <State, Action>(
+	props: ComponentProps<State, Action> & { children?: React.ReactNode }
+) => React.ReactElement<any> = (props) => {
+	const { reducer, state, middleware, context } = props.store;
+
+	return (
+		<context.Provider value={useStore(reducer, state, middleware)}>
+			{props.children}
+		</context.Provider>
+	);
+};
